35c078a47534101a7470c2b38df0e06c972f8411
shahbaz <shahbaznadeem654@gmail.com>
2025-03-11 14:23:26 +0500

    working on admin dashboard

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -13,36 +13,29 @@ const TaskList = () => {
             </div>
             <div id='tasklist' className="flex-shrink-0 h-full w-[300px] bg-cyan-600 rounded-xl p-5 overflow-y-auto">
                 <div className="flex justify-between items-center sticky top-0">
-                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
-                    <h4 className='text-sm'>20 feb, 2024</h4>
+                    <h3 className='bg-red-600 px-3 py-1 rounded sm:text-sm text-xs'>High</h3>
+                    <h4 className='sm:text-sm text-xs'>20 feb, 2024</h4>
                 </div>
-                <h2 className='mt-5 text-xl font-bold'>Make a Youtube video</h2>
+                <h2 className='sm:mt-5 mt-2 sm:text-xl text-base font-bold'>Make a Youtube video</h2>
                 <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ea incidunt provident! Cumque illum est ducimus natus culpa, impedit voluptatum earum, recusandae</p>
             </div>
             <div id='tasklist' className="flex-shrink-0 h-full w-[300px] bg-cyan-600 rounded-xl p-5 overflow-y-auto">
                 <div className="flex justify-between items-center sticky top-0">
-                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
-                    <h4 className='text-sm'>20 feb, 2024</h4>
+                    <h3 className='bg-red-600 px-3 py-1 rounded sm:text-sm text-xs'>High</h3>
+                    <h4 className='sm:text-sm text-xs'>20 feb, 2024</h4>
                 </div>
-                <h2 className='mt-5 text-xl font-bold'>Make a Youtube video</h2>
+                <h2 className='sm:mt-5 mt-2 sm:text-xl text-base font-bold'>Make a Youtube video</h2>
                 <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ea incidunt provident! Cumque illum est ducimus natus culpa, impedit voluptatum earum, recusandae</p>
             </div>
             <div id='tasklist' className="flex-shrink-0 h-full w-[300px] bg-cyan-600 rounded-xl p-5 overflow-y-auto">
                 <div className="flex justify-between items-center sticky top-0">
-                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
-                    <h4 className='text-sm'>20 feb, 2024</h4>
+                    <h3 className='bg-red-600 px-3 py-1 rounded sm:text-sm text-xs'>High</h3>
+                    <h4 className='sm:text-sm text-xs'>20 feb, 2024</h4>
                 </div>
-                <h2 className='mt-5 text-xl font-bold'>Make a Youtube video</h2>
+                <h2 className='sm:mt-5 mt-2 sm:text-xl text-base font-bold'>Make a Youtube video</h2>
                 <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ea incidunt provident! Cumque illum est ducimus natus culpa, impedit voluptatum earum, recusandae</p>
             </div>
-            <div id='tasklist' className="flex-shrink-0 h-full w-[300px] bg-cyan-600 rounded-xl p-5 overflow-y-auto">
-                <div className="flex justify-between items-center sticky top-0">
-                    <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
-                    <h4 className='text-sm'>20 feb, 2024</h4>
-                </div>
-                <h2 className='mt-5 text-xl font-bold'>Make a Youtube video</h2>
-                <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ea incidunt provident! Cumque illum est ducimus natus culpa, impedit voluptatum earum, recusandae</p>
-            </div>
+
 
 
         </div>

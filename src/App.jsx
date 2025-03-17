c3e35e9d348038617d092707db535d14a4d99d5d
shahbaz <shahbaznadeem654@gmail.com>
2025-03-17 15:51:42 +0500

    working on functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import Login from './components/Auth/Login'
-import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage';
+import React, { useContext, useEffect, useState } from 'react';
+import Login from './components/Auth/Login';
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
+import AdminDashboard from './components/Dashboard/AdminDashboard';
+import { AuthContext } from './context/AuthProvider';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [logedInUserData, setLogedInUserData] = useState(null)
+  const authData = useContext(AuthContext);
 
-  const [user, setUser] = useState(null)
+  // useEffect(() => {
+  //   if (authData) {
+  //     const loggedInUser = localStorage.getItem("loggedInUser")
+  //     // setUser(loggedInUser.role)
+  //   }
+
+  // }, [authData])
+
 
   const handleLogin = (email, password) => {
-    if (email == "admin@example.com" && password == "123") {
-      console.log("this is admin")
-    } else if (email == "employee1@example.com" && password == "123") {
-      alert("this is user")
-    } else alert("invalid credentials")
-  }
+    if (email === "admin@example.com" && password === "123") {
+      setUser("admin");
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }))
+    } else if (authData?.employees) {
+      const employee = authData.employees.find(({ email: eEmail, password: ePassword }) => 
+        eEmail === email && ePassword === password
+      );
+    
+      employee && (setUser("employee"), localStorage.setItem("loggedInUser", JSON.stringify({ role: "employee" })));
+    }
+    
+     else {
+      alert("Invalid credentials");
+    }
+  };
 
-  handleLogin("admin@example.com", 123)
 
   return (
-    <div className='costomizecostomizeContainer'>
-      {!user ? <Login handleLogin={handleLogin} /> : ""}
-      {/* <Login/> */}
-      {/* <EmployeeDashboard/> */}
-      {/* <AdminDashboard /> */}
+    <div className="customContainer">
+      {!user && <Login handleLogin={handleLogin} />}
+      {user === "admin" && <AdminDashboard />}
+      {user === "employee" && <EmployeeDashboard />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
